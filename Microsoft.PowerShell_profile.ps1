6e0c3d2beb89259d6479be5f68b7646f065025c8
msienkow <83611292+msienkow@users.noreply.github.com>
2022-08-07 19:34:39 -0500

    Added alias for winget upgrade.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,4 +4,7 @@ cls
 winfetch
 Invoke-Expression (&starship init powershell)
 
+Function wingetupgrade {winget upgrade --all}
+
 Set-Alias -Name notepad -Value 'C:\Program Files\Notepad++\notepad++.exe'
+Set-Alias -Name update -Value wingetupgrade 
